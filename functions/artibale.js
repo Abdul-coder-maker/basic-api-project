5efb1dd8766d7cfb211526b2cfad8e514f1815ef
Abdulrahman Nasser <abdulrahman.mhd.anas@pm.me>
2022-04-30 14:56:24 +0400

    create new function

---
+++ functions/artibale.js
@@ -0,0 +1,23 @@
+const Airtable = require("airtable-node");
+require("dotenv").config();
+const airtable = new Airtable({ apiKey: process.env.ARTABLE_API_KEY })
+  .base("appA05ZtqbhTAvzB0")
+  .table("products");
+exports.handler = async (event, context, callback) => {
+  try {
+    const { records } = await airtable.list();
+    const products = records.map((product) => {
+      const { id } = product;
+      const { name, description, price, image } = product.fields;
+      const url = image[0].url;
+      return { id, name, url, price };
+    });
+    return {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      statusCode: 200,
+      body: JSON.stringify(products),
+    };
+  } catch (error) {}
+};
